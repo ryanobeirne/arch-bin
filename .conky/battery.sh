ca42bcab9cb277efdee391ff2d366bdfad9c21fa
Ryan O'Beirne <ryanobeirne@gmail.com>
2019-06-17 17:39:33 -0400

    First commit for fx505

--- .conky/battery.sh
+++ .conky/battery.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-charge_now=$(cat /sys/class/power_supply/BAT0/charge_now)
-charge_full=$(cat /sys/class/power_supply/BAT0/charge_full)
+#charge_now=$(cat /sys/class/power_supply/BAT0/charge_now)
+#charge_full=$(cat /sys/class/power_supply/BAT0/charge_full)
 
-charge_percent=$(awk -v n=$charge_now -v f=$charge_full 'BEGIN {printf "%0.0f", n / f * 100}')
+#charge_percent=$(awk -v n=$charge_now -v f=$charge_full 'BEGIN {printf "%0.0f", n / f * 100}')
 
-printf '%s\n' "$charge_percent"
+#printf '%s\n' "$charge_percent"
+
+cat /sys/class/power_supply/BAT0/capacity
