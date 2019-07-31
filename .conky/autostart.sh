26967dd8526ee4c0369ec9fc1a8f57c7bebfcd14
Ryan O'Beirne <ryanobeirne@gmail.com>
2019-07-30 21:05:14 -0400

    Remove battery and pause

--- .conky/autostart.sh
+++ .conky/autostart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 conky -c ~/.conky/horical.conkyrc --daemonize
-conky -c ~/.conky/conkyrc --daemonize --pause=5
-conky -c ~/.conky/battery.conkyrc --daemonize
+conky -c ~/.conky/conkyrc --daemonize
+#conky -c ~/.conky/battery.conkyrc --daemonize
