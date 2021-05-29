ef71b4ed299f5b3eaa2d57f6daf8f63268edae68
Ryan O'Beirne <ryanobeirne@ryanobeirne.com>
2021-05-29 18:31:37 -0400

    Use lm_sensors for cpu temp

--- bin/cputemp.sh
+++ bin/cputemp.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-temp="$(awk '{sum+=$1;if ($1==0){zeros+=1;}} END {printf "%0.1f", sum / (NR-zeros) / 1000}' /sys/class/hwmon/hwmon*/temp*_input)"
+# temp="$(awk '{sum+=$1;if ($1==0){zeros+=1;}} END {printf "%0.1f", sum / (NR-zeros) / 1000}' /sys/class/hwmon/hwmon*/temp*_input)"
+temp="$(sensors | sed -nE '/CPUTIN:/s/^.*\+([[:digit:]\.]+)°C.*\(.*\).*$/\1/p')"
 
 printf '%s°C\n' "$temp"
